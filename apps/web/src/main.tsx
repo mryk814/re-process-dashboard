9952ac3c68e93bdb2f5cf8392937bb20424c5e26
mryk814 <54519966+mryk814@users.noreply.github.com>
2026-07-20 16:23:29 +0900

    feat: install inference cache before rendering

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { installInferenceFetchCache } from "./inferenceFetchCache";
 import "./styles.css";
+
+installInferenceFetchCache();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
